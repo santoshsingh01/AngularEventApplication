0c8f19026b13bf3c34e3a95bab2c77505f916ad1
SantoshKumar Singh <SantoshKumar.Singh@civica2.com>
2022-09-08 15:56:27 +0530

    Route-deactivation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,8 +6,8 @@ import { Error404Component } from "./errors/404.component";
 import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 
 export const appRoutes : Routes = [
-    { path: 'events', component: EventsListComponent },
-    { path: 'events/new', component: CreateEventComponent },
+    { path: 'events/new', component: CreateEventComponent , canDeactivate: ['canDeactivateCreateEvent']},
+    { path: 'events', component: EventsListComponent },    
     { path: 'events/:id', component: EvenDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component},    
     { path: '', redirectTo: '/events', pathMatch: 'full' }
