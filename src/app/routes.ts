095b714a10a1443fe169d925683fc02b788e4009
SantoshKumar Singh <SantoshKumar.Singh@civica2.com>
2022-09-09 10:55:56 +0530

    Organizing exports with barrels

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,15 +1,23 @@
 import { Routes } from "@angular/router";
-import { EvenDetailsComponent } from "./events/event-details/event-details.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { CreateEventComponent } from "./events/create-event.component"
+
+import {
+    EventsListComponent,
+    EventListResolver,
+    EvenDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator
+} from './events/index'
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventListResolver } from "./events/event-list-resolver.service";
 
 export const appRoutes : Routes = [
     { path: 'events/new', component: CreateEventComponent , canDeactivate: ['canDeactivateCreateEvent']},
     { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} },    
     { path: 'events/:id', component: EvenDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component},    
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    {
+        path: 'user', 
+        loadChildren: () => import('./user/user.module')
+        .then(m=>m.UserModule)
+    }
 ]
